ec5b0471dbc69790ae223a671928fb122bb23440
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2024-01-21 15:04:32 -0300

    Aula de seletores e estado

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -2,9 +2,10 @@ export default function Home() {
   return (
     <div className='p-4 bg-slate-700 text-slate-100 h-screen'>
       {/* PASSAR O CURSOS NO NOME DAS CLASSES PARA VER O CSS RESULTANTE!! */}
-      <h1 className='font-bold text-5xl text-violet-500'>Hello Tailwind</h1>
+      <h1 className='font-bold text-5xl flex items-center gap-3 text-violet-500 before:w-0.5 before:h-6 before:bg-sky-500'>Hello Tailwind</h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
+      <button disabled className='bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 cursor-pointer disabled:cursor-not-allowed disabled:opacity-60'>Sign in</button>
     </div>
   );
 }
