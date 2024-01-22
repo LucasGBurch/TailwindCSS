56589aa1e998e978f1e7f5eafc69e47b7541db0f
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2024-01-22 17:36:51 -0300

    Estrutura de Layout do Projeto

--- tailwind-next/tailwind.config.ts
+++ tailwind-next/tailwind.config.ts
@@ -1,8 +1,9 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [ // Content é a única configuração obrigatória (pelo menos na época do curso, que ainda tinha module.exports em vez de const config tipado)
-    "./src/**/*.tsx",
+  content: [
+    // Content é a única configuração obrigatória (pelo menos na época do curso, que ainda tinha module.exports em vez de const config tipado)
+    './src/**/*.tsx',
     // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,15 +11,19 @@ const config: Config = {
   theme: {
     extend: {
       // É melhor criar estas configurações por aqui do que valores arbitrários com [] nos estilos
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+
       maxWidth: {
         app: '700px',
       },
 
       colors: {
         teste: '#333',
-      }
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
