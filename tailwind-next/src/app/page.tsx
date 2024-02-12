47b94896c7c182c797c2d8b8400beacad1ef5066
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2024-02-12 16:53:37 -0300

    Componentizando o Select

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -3,6 +3,7 @@ import * as Input from '@/components/Input'
 import * as FileInput from '@/components/Form/FileInput'
 import { Mail } from 'lucide-react'
 import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -109,7 +110,11 @@ export default function Home() {
             >
               Country
             </label>
-            <Select />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+              <SelectItem value="mx" text="Mexico" />
+            </Select>
           </div>
 
           <div className="grid gap-3 grid-cols-form pt-5">
@@ -119,7 +124,13 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div></div>
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (PST) (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
           </div>
 
           <div className="grid gap-3 grid-cols-form pt-5">
