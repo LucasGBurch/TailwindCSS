b5f0d0c9e164dbaf70509e7a548bc7ce7815d592
burchlg <lucasburch@hotmail.com>
2024-02-28 14:43:03 -0300

    Aula de criações de efeitos de foco
    Retirando outlines e adicionando focus. Especialmente com efeitos de ring para dar um efeito de contorno além das bordas. Focus visible nos TabItem para aparecer somente ao navegar pelo teclado.

--- tailwind-next/src/components/SettingTabs/TabItem.tsx
+++ tailwind-next/src/components/SettingTabs/TabItem.tsx
@@ -13,9 +13,12 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
+      className="relative group px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700 outline-none"
     >
-      <span>{title}</span>
+      <span className="rounded group-focus-visible:ring-2 ring-violet-400 group-focus-visible:ring-offset-4">
+        {/* focus-visible para aparecer apenas quando navegar pelo teclado */}
+        {title}
+      </span>
 
       {isSelected && (
         <motion.div
