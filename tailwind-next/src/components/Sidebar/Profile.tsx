2546128ec1ef1158c4e5801dc050d024bd38e9f0
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-03-04 13:36:01 -0300

    Implementando tema Dark na Sidebar
    Aplicação de temas dark para componentes de Inputs e para os Componentes da Sidebar.

--- tailwind-next/src/components/Sidebar/Profile.tsx
+++ tailwind-next/src/components/Sidebar/Profile.tsx
@@ -11,8 +11,10 @@ export function Profile() {
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">Lucas Burch</span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Lucas Burch
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           lucasburch@hotmail.com
         </span>
       </div>
